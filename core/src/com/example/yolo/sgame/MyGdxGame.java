d6f4153e2e08b158e19567683beaecb0092b7bc6
YoloHealth <jainswati048@gmail.com>
2017-08-21 16:05:05 +0530

    LibGDX SoundEffects and StreamingMusic

--- core/src/com/example/yolo/sgame/MyGdxGame.java
+++ core/src/com/example/yolo/sgame/MyGdxGame.java
@@ -3,6 +3,8 @@ package com.example.yolo.sgame;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -143,92 +145,173 @@ import sun.rmi.runtime.Log;
 //}
 
 
+//
+//
+//public class MyGdxGame extends ApplicationAdapter implements GestureDetector.GestureListener{
+//
+//    private SpriteBatch batch;
+//    private Texture texture;
+//    private Sprite sprite;
+//    private OrthographicCamera camera;
+//
+//    @Override
+//    public void create() {
+//        batch = new SpriteBatch();
+//        camera = new OrthographicCamera(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+//        texture = new Texture(Gdx.files.internal("images.jpg"));
+//        sprite = new Sprite(texture);
+//        sprite.setPosition(-sprite.getWidth()/2,-sprite.getWidth()/2);
+//        Gdx.input.setInputProcessor(new GestureDetector(this));
+//    }
+//
+//
+//
+//    @Override
+//    public void dispose() {
+//        batch.dispose();
+//        texture.dispose();
+//    }
+//
+//
+//    @Override
+//    public void render() {
+//        Gdx.gl.glClearColor(1,1,1,1);
+//        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//
+//        batch.setProjectionMatrix(camera.combined);
+//        batch.begin();
+//        sprite.draw(batch);
+//        batch.end();
+//    }
+//
+//
+//    @Override
+//    public boolean pan(float x, float y, float deltaX, float deltaY) {
+//        camera.translate(-deltaX,deltaY);
+//        camera.update();
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean touchDown(float x, float y, int pointer, int button) {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean tap(float x, float y, int count, int button) {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean longPress(float x, float y) {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean fling(float velocityX, float velocityY, int button) {
+//        return false;
+//    }
+//
+//
+//
+//    @Override
+//    public boolean panStop(float x, float y, int pointer, int button) {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean zoom(float initialDistance, float distance) {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+//        return false;
+//    }
+//
+//    @Override
+//    public void pinchStop() {
+//
+//    }
+//}
 
 
-public class MyGdxGame extends ApplicationAdapter implements GestureDetector.GestureListener{
+
+public class MyGdxGame extends ApplicationAdapter implements InputProcessor{
+
 
     private SpriteBatch batch;
-    private Texture texture;
-    private Sprite sprite;
-    private OrthographicCamera camera;
+    private Music music;
+    private Sound sound;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        camera = new OrthographicCamera(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-        texture = new Texture(Gdx.files.internal("images.jpg"));
-        sprite = new Sprite(texture);
-        sprite.setPosition(-sprite.getWidth()/2,-sprite.getWidth()/2);
-        Gdx.input.setInputProcessor(new GestureDetector(this));
+        Gdx.input.setInputProcessor(this);
+//        sound = Gdx.audio.newSound(Gdx.files.internal("beach.wav"));
+        music = Gdx.audio.newMusic(Gdx.files.internal("beach.wav"));
+
     }
-
-
 
     @Override
     public void dispose() {
         batch.dispose();
-        texture.dispose();
+        sound.dispose();
+        music.dispose();
     }
-
 
     @Override
     public void render() {
         Gdx.gl.glClearColor(1,1,1,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        batch.setProjectionMatrix(camera.combined);
         batch.begin();
-        sprite.draw(batch);
         batch.end();
     }
 
-
     @Override
-    public boolean pan(float x, float y, float deltaX, float deltaY) {
-        camera.translate(-deltaX,deltaY);
-        camera.update();
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        music.play();
+//        Long id = sound.play();
+//        sound.setVolume(id,0.9f);
+//        sound.setPitch(id,0.5f);
         return true;
     }
 
     @Override
-    public boolean touchDown(float x, float y, int pointer, int button) {
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        music.pause();
+        return true;
+    }
+
+
+    @Override
+    public boolean keyDown(int keycode) {
         return false;
     }
 
     @Override
-    public boolean tap(float x, float y, int count, int button) {
+    public boolean keyUp(int keycode) {
         return false;
     }
 
     @Override
-    public boolean longPress(float x, float y) {
-        return false;
-    }
-
-    @Override
-    public boolean fling(float velocityX, float velocityY, int button) {
+    public boolean keyTyped(char character) {
         return false;
     }
 
 
-
     @Override
-    public boolean panStop(float x, float y, int pointer, int button) {
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
         return false;
     }
 
     @Override
-    public boolean zoom(float initialDistance, float distance) {
+    public boolean mouseMoved(int screenX, int screenY) {
         return false;
     }
 
     @Override
-    public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+    public boolean scrolled(int amount) {
         return false;
-    }
-
-    @Override
-    public void pinchStop() {
-
     }
 }
